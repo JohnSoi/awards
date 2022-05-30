d2358fa5c2759b853e9308db8b98b0e74a2551bd
TelegaStrasti <baigildin.idel@yandex.ru>
2022-05-30 21:29:45 +0500

    Изменил контент на главной странице

--- resources/views/block/for.blade.php
+++ resources/views/block/for.blade.php
@@ -3,35 +3,51 @@
     <div class="container position-relative">
         <div class="border-top-white"></div>
         <div class="section-for__title">
-            <h2 class="section-title text-white mb-0 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
+            <h2 class="section-title text-white mb-0 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"
+                data-wow-offset="30">
                 R.C.AWARDS
             </h2>
         </div>
         <div class="section-for__inner">
             <div class="section-for__left">
-                <div class="section-for__bigtitle text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">Для чего?</div>
-                <div class="section-for__text">
-                    <p class="text text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
-                        Премия призвана выявить наиболее успешные практики, разработанные в креативном секторе нашей страны, обеспечить им поддержку и&nbsp;национальное признание.
-                        <br><br>
-                        Премия позволит отметить и&nbsp;выделить
-                        лучшие креативные достижения страны.
-                        <br><br>
-                        Наградить авторов и&nbsp;с их помощью вывести Россию
-                        в&nbsp;число передовых креативных держав мира.
-
-                        @if(Request::url() === Route('index'))
-                            Премия&nbsp;подводит итоги 2020-2022гг.
-                        @else
-                            Премия&nbsp;подводит итоги 2020-2021гг.
-                        @endif
-
-
-                    </p>
+                <div class="section-for__bigtitle text-white wow fadeInUp" data-wow-duration="0.8s"
+                     data-wow-delay="0.2s" data-wow-offset="30">Для чего?
                 </div>
+                <div class="section-for__text">
+                    @if(Request::url() === Route('index'))
+                        <p class="text text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"
+                           data-wow-offset="30">
+                            Премия Russian Creative Awards ежегодно вручается за вклад в развитие креативных индустрий
+                            страны.
+                            Премия призвана отметить наиболее успешных деятелей, проекты, разработанные в креативном
+                            секторе,
+                            обеспечить им поддержку и национальное признание.
+                            <br><br>
+                            Премия способствует популяризации деятельности представителей креативных индустрий,
+                            укреплению межотраслевых связей креативного сообщества по всей стране, а также формированию
+                            и
+                            развитию современной российской культуры и идентичности.
+                            <br><br>
+                            Лауреатами становятся физические лица и организации, внесшие значимый вклад в развитие
+                            креативных индустрий.
+                        </p>
+                    @else
+                        <p class="text text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"
+                           data-wow-offset="30">
+                            Премия призвана выявить наиболее успешные практики, разработанные в креативном секторе нашей страны,
+                            обеспечить им поддержку и&nbsp;национальное признание.
+                            <br><br>
+                            Премия позволит отметить и&nbsp;выделить
+                            лучшие креативные достижения страны.
+                            <br><br>
+                            Наградить авторов и&nbsp;с их помощью вывести Россию
+                            в&nbsp;число передовых креативных держав мира.
+                        </p>
+                @endif
             </div>
             <div class="section-for__right">
-                <div class="section-for__figure wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
+                <div class="section-for__figure wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"
+                     data-wow-offset="30">
                     <div id="mainspring"></div>
                 </div>
             </div>
@@ -40,11 +56,13 @@
             <div class="section-for__animated">
                 <div class="section-for__animated-figure">
                     <div id="television"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg" style="opacity: 0; visibility: hidden; position:absolute; bottom: 0; left: 0; z-index: -1;">
+                    <svg xmlns="http://www.w3.org/2000/svg"
+                         style="opacity: 0; visibility: hidden; position:absolute; bottom: 0; left: 0; z-index: -1;">
                         <defs>
-                            <linearGradient id="violet_gradient" x1="0%" y1="0%" x2="0%" y2="100%" gradientUnits="userSpaceOnUse">
-                                <stop offset="0%" stop-color="#1E1E1E" />
-                                <stop offset="100%" stop-color="#C3A5F2" />
+                            <linearGradient id="violet_gradient" x1="0%" y1="0%" x2="0%" y2="100%"
+                                            gradientUnits="userSpaceOnUse">
+                                <stop offset="0%" stop-color="#1E1E1E"/>
+                                <stop offset="100%" stop-color="#C3A5F2"/>
                             </linearGradient>
                         </defs>
                     </svg>
@@ -56,21 +74,28 @@
                 </div>
             </div>
             <div class="section-for__author">
-                <div class="section-for__avatar wow fadeIn" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
+                <div class="section-for__avatar wow fadeIn" data-wow-duration="0.8s" data-wow-delay="0.2s"
+                     data-wow-offset="30">
                     <picture>
                         <source srcset="/img/V2/page/main/card/gradient/avatar.png" media="(min-width: 768px)">
                         <img data-src="/img/V2/page/main/card/gradient/avatar-sm.png" class="lazyload">
                     </picture>
                 </div>
                 <div class="section-for__quote">
-                    <div class="section-for__quote-text text text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
-                        «Мне очень нравится идея, которая родилась на&nbsp;Российской креативной неделе, о&nbsp;том, чтобы сделать в&nbsp;следующем году первую российскую национальную премию за&nbsp;успехи и&nbsp;достижения в&nbsp;креативных индустриях».
+                    <div class="section-for__quote-text text text-white wow fadeInUp" data-wow-duration="0.8s"
+                         data-wow-delay="0.2s" data-wow-offset="30">
+                        «Мне очень нравится идея, которая родилась на&nbsp;Российской креативной неделе, о&nbsp;том,
+                        чтобы сделать в&nbsp;следующем году первую российскую национальную премию за&nbsp;успехи и&nbsp;достижения
+                        в&nbsp;креативных индустриях».
                     </div>
-                    <div class="section-for__quote-name text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s" data-wow-offset="30">
+                    <div class="section-for__quote-name text-white wow fadeInUp" data-wow-duration="0.8s"
+                         data-wow-delay="0.3s" data-wow-offset="30">
                         Сергей Кириенко
                     </div>
-                    <div class="section-for__quote-devider devider devider-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s" data-wow-offset="30"></div>
-                    <div class="section-for__quote-footer text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s" data-wow-offset="30">
+                    <div class="section-for__quote-devider devider devider-white wow fadeInUp" data-wow-duration="0.8s"
+                         data-wow-delay="0.4s" data-wow-offset="30"></div>
+                    <div class="section-for__quote-footer text-white wow fadeInUp" data-wow-duration="0.8s"
+                         data-wow-delay="0.5s" data-wow-offset="30">
                         Первый заместитель руководителя <span class="d-md-inline d-sm-inline d-none"><br></span>
                         Администрации Президента РФ
                     </div>
