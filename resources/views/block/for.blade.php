9b72e82b411723835b4a7307b19ced424dfed752
TelegaStrasti <baigildin.idel@yandex.ru>
2022-05-30 21:40:55 +0500

    Пофиксил вывод года

--- resources/views/block/for.blade.php
+++ resources/views/block/for.blade.php
@@ -3,16 +3,13 @@
     <div class="container position-relative">
         <div class="border-top-white"></div>
         <div class="section-for__title">
-            <h2 class="section-title text-white mb-0 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"
-                data-wow-offset="30">
+            <h2 class="section-title text-white mb-0 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
                 R.C.AWARDS
             </h2>
         </div>
         <div class="section-for__inner">
             <div class="section-for__left">
-                <div class="section-for__bigtitle text-white wow fadeInUp" data-wow-duration="0.8s"
-                     data-wow-delay="0.2s" data-wow-offset="30">Для чего?
-                </div>
+                <div class="section-for__bigtitle text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">Для чего?</div>
                 <div class="section-for__text">
                     @if(Request::url() === Route('index'))
                         <p class="text text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"
@@ -42,12 +39,17 @@
                             <br><br>
                             Наградить авторов и&nbsp;с их помощью вывести Россию
                             в&nbsp;число передовых креативных держав мира.
+                            @if(Request::url() === Route('index'))
+                                Премия&nbsp;подводит итоги 2020-2022гг.
+                            @else
+                                Премия&nbsp;подводит итоги 2020-2021гг.
+                            @endif
                         </p>
-                @endif
+                    @endif
+                </div>
             </div>
             <div class="section-for__right">
-                <div class="section-for__figure wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s"
-                     data-wow-offset="30">
+                <div class="section-for__figure wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
                     <div id="mainspring"></div>
                 </div>
             </div>
@@ -56,13 +58,11 @@
             <div class="section-for__animated">
                 <div class="section-for__animated-figure">
                     <div id="television"></div>
-                    <svg xmlns="http://www.w3.org/2000/svg"
-                         style="opacity: 0; visibility: hidden; position:absolute; bottom: 0; left: 0; z-index: -1;">
+                    <svg xmlns="http://www.w3.org/2000/svg" style="opacity: 0; visibility: hidden; position:absolute; bottom: 0; left: 0; z-index: -1;">
                         <defs>
-                            <linearGradient id="violet_gradient" x1="0%" y1="0%" x2="0%" y2="100%"
-                                            gradientUnits="userSpaceOnUse">
-                                <stop offset="0%" stop-color="#1E1E1E"/>
-                                <stop offset="100%" stop-color="#C3A5F2"/>
+                            <linearGradient id="violet_gradient" x1="0%" y1="0%" x2="0%" y2="100%" gradientUnits="userSpaceOnUse">
+                                <stop offset="0%" stop-color="#1E1E1E" />
+                                <stop offset="100%" stop-color="#C3A5F2" />
                             </linearGradient>
                         </defs>
                     </svg>
@@ -74,28 +74,21 @@
                 </div>
             </div>
             <div class="section-for__author">
-                <div class="section-for__avatar wow fadeIn" data-wow-duration="0.8s" data-wow-delay="0.2s"
-                     data-wow-offset="30">
+                <div class="section-for__avatar wow fadeIn" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
                     <picture>
                         <source srcset="/img/V2/page/main/card/gradient/avatar.png" media="(min-width: 768px)">
                         <img data-src="/img/V2/page/main/card/gradient/avatar-sm.png" class="lazyload">
                     </picture>
                 </div>
                 <div class="section-for__quote">
-                    <div class="section-for__quote-text text text-white wow fadeInUp" data-wow-duration="0.8s"
-                         data-wow-delay="0.2s" data-wow-offset="30">
-                        «Мне очень нравится идея, которая родилась на&nbsp;Российской креативной неделе, о&nbsp;том,
-                        чтобы сделать в&nbsp;следующем году первую российскую национальную премию за&nbsp;успехи и&nbsp;достижения
-                        в&nbsp;креативных индустриях».
+                    <div class="section-for__quote-text text text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="30">
+                        «Мне очень нравится идея, которая родилась на&nbsp;Российской креативной неделе, о&nbsp;том, чтобы сделать в&nbsp;следующем году первую российскую национальную премию за&nbsp;успехи и&nbsp;достижения в&nbsp;креативных индустриях».
                     </div>
-                    <div class="section-for__quote-name text-white wow fadeInUp" data-wow-duration="0.8s"
-                         data-wow-delay="0.3s" data-wow-offset="30">
+                    <div class="section-for__quote-name text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.3s" data-wow-offset="30">
                         Сергей Кириенко
                     </div>
-                    <div class="section-for__quote-devider devider devider-white wow fadeInUp" data-wow-duration="0.8s"
-                         data-wow-delay="0.4s" data-wow-offset="30"></div>
-                    <div class="section-for__quote-footer text-white wow fadeInUp" data-wow-duration="0.8s"
-                         data-wow-delay="0.5s" data-wow-offset="30">
+                    <div class="section-for__quote-devider devider devider-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.4s" data-wow-offset="30"></div>
+                    <div class="section-for__quote-footer text-white wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.5s" data-wow-offset="30">
                         Первый заместитель руководителя <span class="d-md-inline d-sm-inline d-none"><br></span>
                         Администрации Президента РФ
                     </div>
