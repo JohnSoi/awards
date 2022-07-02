f4c84a1e0ee7711ebcb21ca0e2aeb2a8955761ce
JohnSoi <un.perso@yandex.ru>
2022-07-02 20:05:49 +0500

    Доработал график мероприятий

--- resources/views/block/history.blade.php
+++ resources/views/block/history.blade.php
@@ -8,72 +8,55 @@
         </div>
     </div>
     <div class="section-history__content section-history__content-slider">
-        <div cursor-status class="swiper-container swiper-history wow fadeIn" data-wow-duration="0.8s"
-             data-wow-delay="0.2s">
-            <div class="swiper-wrapper" data-white>
-                <div class="swiper-slide">
-                    <div class="card-timeline">
-                        <div class="section-history__line"></div>
-                        <div class="card-timeline__date">07.07</div>
-                        <div class="card-timeline__point"></div>
-                        <div class="card-timeline__desc">Старт Russian Creative Week</div>
+        <div class="container section-history__wrapper wow fadeIn" data-wow-duration="0.8s"
+             data-wow-delay="0.2s" data-white>
+            <div class="section-history__item">
+                <div class="card-timeline">
+                    <div class="card-timeline__date">07.07</div>
+                    <div
+                            class="card-timeline__point
+                                   card-timeline__point-{{ \App\Helpers\TimelineHelpers::getClassPrefixHistoryPoint('28.06.2022') }}"
+                            title="{{ \App\Helpers\TimelineHelpers::getTitleHistoryPoint('28.06.2022') }}"
+                    >
                     </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card-timeline">
-                        <div class="section-history__line"></div>
-                        <div class="card-timeline__date">07.07 – 25.09</div>
-                        <div class="card-timeline__point"></div>
-                        <div class="card-timeline__desc">Прием заявок</div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card-timeline">
-                        <div class="section-history__line"></div>
-                        <div class="card-timeline__date">03.10 – 30.10</div>
-                        <div class="card-timeline__point"></div>
-                        <div class="card-timeline__desc">Работа экспертов</div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card-timeline">
-                        <div class="section-history__line"></div>
-                        <div class="card-timeline__date">07.11 – 27.11</div>
-                        <div class="card-timeline__point"></div>
-                        <div class="card-timeline__desc">Работа жюри</div>
-                    </div>
-                </div>
-                <div class="swiper-slide">
-                    <div class="card-timeline">
-                        <div class="section-history__line"></div>
-                        <div class="card-timeline__date">09.12</div>
-                        <div class="card-timeline__point"></div>
-                        <div class="card-timeline__desc">Торжественная церемония вручения премии
-                        </div>
-                    </div>
+                    <div class="card-timeline__desc">Старт Russian Creative Week</div>
                 </div>
             </div>
-            <div class="slider-navigation slider-navigation__white pl-sm-0 pl-md-0">
-                <div class="arrows-nav">
-                    <div class="arrows-nav__left">
-                        <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="23" cy="23" r="22.5" transform="rotate(-180 23 23)" stroke="#FBFBFB"/>
-                            <path d="M11.6464 22.6464C11.4512 22.8417 11.4512 23.1583 11.6464 23.3536L14.8284 26.5355C15.0237 26.7308 15.3403 26.7308 15.5355 26.5355C15.7308 26.3403 15.7308 26.0237 15.5355 25.8284L12.7071 23L15.5355 20.1716C15.7308 19.9763 15.7308 19.6597 15.5355 19.4645C15.3403 19.2692 15.0237 19.2692 14.8284 19.4645L11.6464 22.6464ZM34 22.5L12 22.5L12 23.5L34 23.5L34 22.5Z"
-                                  fill="#FBFBFB"/>
-                        </svg>
-                    </div>
-                    <div class="arrows-nav__right">
-                        <svg width="46" height="46" viewBox="0 0 46 46" fill="none" xmlns="http://www.w3.org/2000/svg">
-                            <circle cx="23" cy="23" r="22.5" stroke="#FBFBFB"/>
-                            <path d="M34.3536 23.3536C34.5488 23.1583 34.5488 22.8417 34.3536 22.6464L31.1716 19.4645C30.9763 19.2692 30.6597 19.2692 30.4645 19.4645C30.2692 19.6597 30.2692 19.9763 30.4645 20.1716L33.2929 23L30.4645 25.8284C30.2692 26.0237 30.2692 26.3403 30.4645 26.5355C30.6597 26.7308 30.9763 26.7308 31.1716 26.5355L34.3536 23.3536ZM12 23.5H34V22.5H12V23.5Z"
-                                  fill="#FBFBFB"/>
-                        </svg>
-                    </div>
+            <div class="section-history__item">
+                <div class="card-timeline">
+                    <div class="card-timeline__date">07.07 – 25.09</div>
+                    <div class="card-timeline__point
+                                   card-timeline__point-{{ \App\Helpers\TimelineHelpers::getClassPrefixHistoryPoint('07.07.2022', '25.09.2022') }}"
+                            title="{{ \App\Helpers\TimelineHelpers::getTitleHistoryPoint('07.07.2022', '25.09.2022') }}"></div>
+                    <div class="card-timeline__desc">Прием заявок</div>
                 </div>
-                <div class="counter">
-                    <span data-active-count>1</span>
-                    /
-                    <span data-all-count></span>
+            </div>
+            <div class="section-history__item">
+                <div class="card-timeline">
+                    <div class="card-timeline__date">03.10 – 30.10</div>
+                    <div class="card-timeline__point
+                                   card-timeline__point-{{ \App\Helpers\TimelineHelpers::getClassPrefixHistoryPoint('03.10.2022', '30.10.2022') }}"
+                            title="{{ \App\Helpers\TimelineHelpers::getTitleHistoryPoint('03.10.2022', '30.10.2022') }}"></div>
+                    <div class="card-timeline__desc">Работа экспертов</div>
+                </div>
+            </div>
+            <div class="section-history__item">
+                <div class="card-timeline">
+                    <div class="card-timeline__date">07.11 – 27.11</div>
+                    <div class="card-timeline__point
+                                   card-timeline__point-{{ \App\Helpers\TimelineHelpers::getClassPrefixHistoryPoint('07.11.2022', '27.11.2022') }}"
+                            title="{{ \App\Helpers\TimelineHelpers::getTitleHistoryPoint('07.11.2022', '27.11.2022') }}"></div>
+                    <div class="card-timeline__desc">Работа жюри</div>
+                </div>
+            </div>
+            <div class="section-history__item">
+                <div class="card-timeline">
+                    <div class="card-timeline__date">09.12</div>
+                    <div class="card-timeline__point
+                                   card-timeline__point-{{ \App\Helpers\TimelineHelpers::getClassPrefixHistoryPoint('09.12.2022') }}"
+                            title="{{ \App\Helpers\TimelineHelpers::getTitleHistoryPoint('09.12.2022') }}"></div>
+                    <div class="card-timeline__desc">Торжественная церемония вручения премии
+                    </div>
                 </div>
             </div>
         </div>
