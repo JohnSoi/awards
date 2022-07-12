d770a0b5597d164a3ad2964f7896d54856797f47
JohnSoi <un.perso@yandex.ru>
2022-07-12 21:07:22 +0500

    Правка страницы восстановления

--- webpack.mix.js
+++ webpack.mix.js
@@ -35,6 +35,7 @@ mix
     .js('resources/js/page/auth/login.js', 'js/page/auth')
     .js('resources/js/page/auth/register.js', 'js/page/auth')
     .js('resources/js/page/photos/index.js', 'js/page/photos')
+    .js('resources//js/page/auth/passwords/email.js', '/js/page/auth/passwords')
     .sass('resources/scss/app.scss', 'css')
     .sass('resources/scss/new-design.scss', 'css')
     .sass('resources/scss/page/main/index.scss', 'css/page/main')
