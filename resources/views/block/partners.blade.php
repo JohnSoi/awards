23ae8f04ccd4ebdaeb9a63446179caada618b991
JohnSoi <un.perso@yandex.ru>
2022-07-21 22:09:30 +0500

    Выкладка на бой

--- resources/views/block/partners.blade.php
+++ resources/views/block/partners.blade.php
@@ -18,7 +18,7 @@
                 </div>
             </div>
             <div class="border-top-black"></div>
-            <h2  class="section-title mt-4 mb-0 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="0">{{ __('main.partners') }}</h2>
+            <h2 style="display: none" class="section-title mt-4 mb-0 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="0">{{ __('main.partners') }}</h2>
             <div  class="section-partners__content">
                 <div class="row partners-stripe">
                     <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 partner-img wow fadeIn" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="0">
@@ -50,7 +50,7 @@
             </div>
             <div class="border-top-black"></div>
             <h2 class="section-title mb-0 mt-4 wow fadeInUp" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="0">{{ __('main.partner_inf2') }}</h2>
-            <div class="section-partners__content">
+            <div style="display: none" class="section-partners__content">
                 <div class="row partners-stripe">
                     <div class="col-xl-2 col-lg-3 col-md-4 col-sm-4 col-6 partner-img wow fadeIn" data-wow-duration="0.8s" data-wow-delay="0.2s" data-wow-offset="0">
                         <a href="https://tass.ru/" target="_blank"><img data-src="./../../../img/V2/page/main/partners/tasslogo.svg" class="lazyload"></a>
